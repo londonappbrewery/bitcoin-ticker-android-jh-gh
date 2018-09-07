fd82eb83e7a48e27ff066ab3afa6773d149ef3f2
John Hartmann <John.Hartmann@solnetsolutions.co.nz>
2018-09-08 11:10:47 +1200

    Finished project

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,19 +12,25 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
-
+//    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
     // Member Variables:
     TextView mPriceTextView;
+    String mCurrency = "NZD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +50,57 @@ public class MainActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
-        // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mCurrency = (String) parent.getSelectedItem();
+                getPrice();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        getPrice();
     }
 
     // TODO: complete the letsDoSomeNetworking() method
-    private void letsDoSomeNetworking(String url) {
+    private void getPrice() {
+        String url = BASE_URL + mCurrency;
+        AsyncHttpClient client = new AsyncHttpClient();
+        debug("Initial connect timeout: " + client.getConnectTimeout());
+        client.setConnectTimeout(1000);
+        debug("New connect timeout: " + client.getConnectTimeout());
+        RequestParams params = new RequestParams();
+        debug("Calling url: " + url);
+        mPriceTextView.setText(R.string.label_default_text);
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                debug("onSuccess called");
+                double last;
+                try {
+                    last = response.getDouble("last");
+                    mPriceTextView.setText(String.valueOf(last));
+                } catch (JSONException e) {
+                    error("Failed to parse response", e);
+//                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                error("onFailure called statusCode: " + statusCode, throwable);
+                mPriceTextView.setText(R.string.label_error_text);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                error("onFailure called statusCode: " + statusCode, throwable);
+                mPriceTextView.setText(R.string.label_error_text);
+            }
+        });
 
 //        AsyncHttpClient client = new AsyncHttpClient();
 //        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
@@ -71,9 +122,20 @@ public class MainActivity extends AppCompatActivity {
 //                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
 //            }
 //        });
-
-
     }
 
+    private void error(String message, Throwable throwable) {
+        Log.e("bitcointicker", message);
+        throwable.printStackTrace();
+        showShortToast(message);
+    }
+
+    private void showShortToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    void debug(String message) {
+        Log.d("bitcointicker", message);
+    }
 
 }
